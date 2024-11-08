f1c26a670a2f66dd1baa3b8e982ee294a1b40216
William <william@fayers.com>
2024-11-08 13:25:25 +0000

    fix: circular dependency

--- source/utilities/print_utilities.lua
+++ source/utilities/print_utilities.lua
@@ -2,9 +2,6 @@
 --- @module "utilities.print_utilities"
 
 local constants = require("config.constants")
-local logging = require("modules.logging")
-
-local logger = logging.get_logger("print_utilities", "print_utilities.log")
 
 
 local print_utilities = {}
@@ -14,17 +11,13 @@ local print_utilities = {}
 --- @param text string The text to print.
 --- @param colour number|nil The colour to print the text in, or nil to use the default text colour.
 function print_utilities.pretty_print(text, colour)
-    logger.trace("Pretty printing text: " .. text)
     if colour == nil then
-        logger.trace("No colour specified, using default text colour")
         colour = constants.DEFAULT_TEXT_COLOUR
     end
 
     term.setTextColor(colour)
-    logger.debug("Printing text: " .. text)
     print(text)
     term.setTextColor(constants.DEFAULT_TEXT_COLOUR)
-    logger.trace("Text printed: " .. text)
 end
 
 
